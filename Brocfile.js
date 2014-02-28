6cf008a8120b70028a0161b9e3afbc28b6fad626
Ryan Florence <rpflorence@gmail.com>
2014-02-28 15:20:37 -0700

    update broccoli-dist-es6-module

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,11 @@ module.exports = function(broccoli) {
   return require('broccoli-dist-es6-module')(broccoli.makeTree('lib'), {
     global: 'emq',
     packageName: 'ember-qunit',
-    main: 'main'
+    main: 'main',
+    shim: {
+      'ember': 'Ember',
+      'qunit': 'QUnit'
+    }
   });
 };
 
