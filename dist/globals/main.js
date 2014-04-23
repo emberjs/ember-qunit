fcddf2304baab2152fb39c83a1de87dfce594dce
Whitney Young <wbyoung@fadingred.com>
2014-04-23 16:46:27 -0700

    Started abstraction.

--- dist/globals/main.js
+++ dist/globals/main.js
@@ -122,88 +122,99 @@ var Ember = window.Ember["default"] || window.Ember;
 var testContext = _dereq_("./test-context")["default"] || _dereq_("./test-context");
 var isolatedContainer = _dereq_("./isolated-container")["default"] || _dereq_("./isolated-container");
 
-exports["default"] = function moduleFor(fullName, description, callbacks, delegate) {
-  var container;
-  var context;
-  
-  var _callbacks = {
-    setup: function(){
-      callbacks = callbacks || { };
-
-      var needs = [fullName].concat(callbacks.needs || []);
-      container = isolatedContainer(needs);
-
-      callbacks.subject   = callbacks.subject || defaultSubject;
-
-      callbacks.setup     = callbacks.setup    || function() { };
-      callbacks.teardown  = callbacks.teardown || function() { };
-      
-      function factory() {
-        return container.lookupFactory(fullName);
-      }
-      
-      testContext.set({
-        container:            container,
-        factory:              factory,
-        dispatcher:           null,
-        __setup_properties__: callbacks
-      });
-      
-      context = testContext.get();
-
-      if (delegate) {
-        delegate(container, context, defaultSubject);
-      }
-      
-      if (Ember.$('#ember-testing').length === 0) {
-        Ember.$('<div id="ember-testing"/>').appendTo(document.body);
-      }
-      
-      buildContextVariables(context);
-      callbacks.setup.call(context, container);
-    },
-
-    teardown: function(){
-      Ember.run(function(){
-        container.destroy();
-        
-        if (context.dispatcher) {
-          context.dispatcher.destroy();
-        }
-      });
-      
-      callbacks.teardown(container);
-      Ember.$('#ember-testing').empty();
-    }
+function builder(fullName, needs) {
+  var container = isolatedContainer([fullName].concat(needs || []));
+  var factory = function() {
+    return container.lookupFactory(fullName);
   };
+  return {
+    container: container,
+    factory: factory
+  };
+};
 
-  QUnit.module(description || fullName, _callbacks);
-}
-
-function defaultSubject(options, factory) {
-  return factory.create(options);
-}
-
-// allow arbitrary named factories, like rspec let
-function buildContextVariables(context) {
-  var cache     = { };
-  var callbacks = context.__setup_properties__;
-  var container = context.container;
-  var factory   = context.factory;
+function qunitModule(builder, delegate) {
+  return function moduleFor(fullName, description, callbacks, delegate) {
+    var products;
+    var context;
     
-  Ember.keys(callbacks).filter(function(key){
-    // ignore the default setup/teardown keys
-    return key !== 'setup' && key !== 'teardown';
-  }).forEach(function(key){
-    context[key] = function(options) {
-      if (cache[key]) { return cache[key]; }
+    var _callbacks = {
+      setup: function(){
+        callbacks = callbacks || { };
+        callbacks.subject   = callbacks.subject || defaultSubject;
 
-      var result = callbacks[key](options, factory(), container);
-      cache[key] = result;
-      return result;
+        callbacks.setup     = callbacks.setup    || function() { };
+        callbacks.teardown  = callbacks.teardown || function() { };
+        
+        products = builder(fullName, callbacks.needs);
+
+        testContext.set({
+          container:            products.container,
+          factory:              products.factory,
+          dispatcher:           null,
+          __setup_properties__: callbacks
+        });
+        
+        context = testContext.get();
+
+        if (delegate) {
+          delegate(products.container, context, defaultSubject);
+        }
+        
+        if (Ember.$('#ember-testing').length === 0) {
+          Ember.$('<div id="ember-testing"/>').appendTo(document.body);
+        }
+        
+        buildContextVariables(context);
+        callbacks.setup.call(context, products.container);
+      },
+
+      teardown: function(){
+        Ember.run(function(){
+          products.container.destroy();
+          
+          if (context.dispatcher) {
+            context.dispatcher.destroy();
+          }
+        });
+        
+        callbacks.teardown(products.container);
+        Ember.$('#ember-testing').empty();
+      }
     };
-  });
+
+    QUnit.module(description || fullName, _callbacks);
+  }
+
+  function defaultSubject(options, factory) {
+    return factory.create(options);
+  }
+
+  // allow arbitrary named factories, like rspec let
+  function buildContextVariables(context) {
+    var cache     = { };
+    var callbacks = context.__setup_properties__;
+    var container = context.container;
+    var factory   = context.factory;
+      
+    Ember.keys(callbacks).filter(function(key){
+      // ignore the default setup/teardown keys
+      return key !== 'setup' && key !== 'teardown';
+    }).forEach(function(key){
+      context[key] = function(options) {
+        if (cache[key]) { return cache[key]; }
+
+        var result = callbacks[key](options, factory(), container);
+        cache[key] = result;
+        return result;
+      };
+    });
+  }
 }
+
+exports["default"] = qunitModule(builder, null);
+exports.builder = builder;
+exports.qunitModule = qunitModule;
 },{"./isolated-container":1,"./test-context":6}],6:[function(_dereq_,module,exports){
 "use strict";
 var __test_context__;
