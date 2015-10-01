bd485fee3fc8f1f7073e9a90fc91eff5372eadb3
Robert Jackson <robert.w.jackson@me.com>
2015-10-01 11:15:04 -0400

    Ensure `ember release` works when builds repo doesn't exist.

--- config/release.js
+++ config/release.js
@@ -1,6 +1,7 @@
 /* jshint node:true */
 
 var execSync = require('child_process').execSync;
+var existsSync = require('exists-sync');
 
 function run(command) {
   console.log('running: "' + command + '"');
@@ -12,6 +13,11 @@ module.exports = {
   // Publish the new release to NPM after a successful push
   afterPush: function(project, versions) {
     run('ember build');
+
+    if (!existsSync('../ember-qunit-builds')) {
+      run('cd .. && git clone git@github.com:rwjblue/ember-qunit-builds.git');
+    }
+
     run('cp ./build/*.* ../ember-qunit-builds/');
     run('cd ../ember-qunit-builds && git add --all');
     run('cd ../ember-qunit-builds && git commit --message="Release ' + versions.next + '"');
