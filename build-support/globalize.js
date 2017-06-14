39beb9706e7836cd86cd81b2ea07e2b02a7f80c0
Tomster <tomster@emberjs.com>
2017-06-14 12:54:57 -0400

    FIX: A typo was preventing the globalize build from working

--- build-support/globalize.js
+++ build-support/globalize.js
@@ -2,7 +2,7 @@ define("ember", ["exports"], function(__exports__) {
   __exports__["default"] = window.Ember;
 });
 
-var emberQunit = requireModule("ember-qunit");
+var emberQUnit = requireModule("ember-qunit");
 
 for (var exportName in emberQUnit) {
   window[exportName] = emberQUnit[exportName];
