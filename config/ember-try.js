9d4546abbe49884ec71eeab04141aaad9a7664f4
Robert Jackson <rjackson@linkedin.com>
2017-10-07 15:46:10 -0400

    Update .travis.yml and config/ember-try.js to test 2.0 and 2.4.

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,6 +2,35 @@
 module.exports = {
   scenarios: [
     {
+      name: 'ember-2.0',
+      bower: {
+        dependencies: {
+          ember: '~2.0.0',
+        },
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null,
+        },
+      },
+    },
+    {
+      name: 'ember-lts-2.4',
+      bower: {
+        dependencies: {
+          ember: 'components/ember#lts-2-4',
+        },
+        resolutions: {
+          ember: 'lts-2-4',
+        },
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null,
+        },
+      },
+    },
+    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
