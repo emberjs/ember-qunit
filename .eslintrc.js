9cc8f47b2ac6a949b0a95c5e862cb2a7ceba2e48
Robert Jackson <rjackson@linkedin.com>
2017-10-17 14:20:13 -0400

    Use a single centralized .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,34 @@ module.exports = {
   plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
   },
   rules: {
-    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5' }],
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      trailingComma: 'es5',
+      printWidth: 100,
+    }],
   },
+  overrides: [
+    {
+      files: ['./index.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      }
+    },
+    {
+      files: ['tests/**/*.js'],
+      env: {
+        qunit: true
+      }
+    },
+  ]
 };
