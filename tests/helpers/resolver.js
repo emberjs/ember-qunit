449d963aadb81d94d7f8f5dddbf23714dd491520
Robert Jackson <rjackson@linkedin.com>
2017-12-16 13:40:39 -0500

    Refactor test setup to use `setApplication`.

--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -1,28 +1,18 @@
-import Ember from 'ember';
-import AppResolver from '../../resolver';
+import App from '../../app';
+import { setRegistry } from '../../resolver';
 import config from '../../config/environment';
 import { setResolver } from 'ember-test-helpers';
 
-const Resolver = AppResolver.extend({
-  resolve: function(fullName) {
-    return this.registry[fullName] || this._super.apply(this, arguments);
-  },
-
-  normalize: function(fullName) {
-    return Ember.String.dasherize(fullName);
-  },
+export const application = App.create(config.APP);
+export const resolver = application.Resolver.create({
+  namespace: application,
+  isResolverFromTestHelpers: true,
 });
-const resolver = Resolver.create();
-
-resolver.namespace = {
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-};
 
 export default resolver;
 
 setResolver(resolver);
 
 export function setResolverRegistry(registry) {
-  resolver.set('registry', registry);
+  setRegistry(registry);
 }
