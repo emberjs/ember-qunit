f80fcff5be973fdeda78324ef7f217550b41348e
Robert Jackson <rjackson@linkedin.com>
2017-12-17 21:59:01 -0500

    Add validation for Ember.onerror in testing.

---
+++ tests/unit/setup-ember-onerror-validation-test.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+import { module } from 'qunit';
+import { setupEmberOnerrorValidation } from 'ember-qunit';
+
+module('setupEmberOnerrorValidation', function(hooks) {
+  hooks.beforeEach(function(assert) {
+    let originalPushResult = assert.pushResult;
+    assert.pushResult = function(resultInfo) {
+      // Inverts the result so we can test failing assertions
+      resultInfo.result = !resultInfo.result;
+      resultInfo.message = `Failed: ${resultInfo.message}`;
+      originalPushResult(resultInfo);
+    };
+
+    this.originalEmberOnerror = Ember.onerror;
+    Ember.onerror = function() {
+      // intentionally swallowing here
+    };
+  });
+
+  hooks.afterEach(function() {
+    Ember.onerror = this.originalEmberOnerror;
+  });
+
+  setupEmberOnerrorValidation();
+});
