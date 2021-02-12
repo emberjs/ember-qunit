72b399591d78c6d79cbeca981455a4a9d9db55d5
Andrew A. Lee <andlee@linkedin.com>
2021-02-12 08:59:54 -0800

    Fixes testing container layout issues for QUnit 2.14

--- addon-test-support/qunit-configuration.js
+++ addon-test-support/qunit-configuration.js
@@ -3,7 +3,6 @@ import * as QUnit from 'qunit';
 QUnit.config.autostart = false;
 QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
 QUnit.config.urlConfig.push({ id: 'nolint', label: 'Disable Linting' });
-QUnit.config.urlConfig.push({ id: 'dockcontainer', label: 'Dock container' });
 QUnit.config.urlConfig.push({ id: 'devmode', label: 'Development mode' });
 
 QUnit.config.testTimeout = QUnit.urlParams.devmode ? null : 60000; //Default Test Timeout 60 Seconds
