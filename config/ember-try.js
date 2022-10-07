de6ad720fd2ecf2d99aae506f34e229cd77c5b83
Chris Krycho <hello@chriskrycho.com>
2022-10-07 15:45:54 -0600

    Drop support for jQuery and Classic

--- config/ember-try.js
+++ config/ember-try.js
@@ -93,24 +93,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
-      {
         name: 'ember-default',
         npm: {
           devDependencies: {},
