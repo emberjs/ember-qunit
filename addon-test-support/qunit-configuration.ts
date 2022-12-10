a11495e55eb3c861bbfed1d9f2120c41b3cb9184
Krystan HuffMenne <krystan@tilde.io>
2022-12-09 16:30:35 -0800

    Fix type checking errors for qunit-configuration

--- addon-test-support/qunit-configuration.ts
+++ addon-test-support/qunit-configuration.ts
@@ -5,4 +5,6 @@ QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
 QUnit.config.urlConfig.push({ id: 'nolint', label: 'Disable Linting' });
 QUnit.config.urlConfig.push({ id: 'devmode', label: 'Development mode' });
 
+// @ts-expect-error FIXME qunit types are incorrect
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 QUnit.config.testTimeout = QUnit.urlParams.devmode ? null : 60000; //Default Test Timeout 60 Seconds
