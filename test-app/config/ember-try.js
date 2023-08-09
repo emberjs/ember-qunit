d9c84631cc9f4e3ca54170c44dd68b1a169ec033
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-08 21:10:31 -0400

    Use correct ember-cli now that fixes have been backported, and add ember 4.12 to ember-try

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -12,6 +12,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
+            'ember-cli': '~4.8.1',
           },
         },
       },
@@ -19,7 +20,17 @@ module.exports = async function () {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
+            'ember-source': '~4.8.0',
+            'ember-cli': '~4.8.1',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
+            'ember-cli': '~4.12.2',
           },
         },
       },
@@ -31,6 +42,7 @@ module.exports = async function () {
           },
           devDependencies: {
             'ember-source': await getChannelURL('release'),
+            'ember-cli': '^5.1.0',
           },
         },
       },
@@ -42,6 +54,7 @@ module.exports = async function () {
           },
           devDependencies: {
             'ember-source': await getChannelURL('beta'),
+            'ember-cli': '^5.1.0',
           },
         },
       },
@@ -53,6 +66,7 @@ module.exports = async function () {
           },
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
+            'ember-cli': '^5.1.0',
           },
         },
       },
@@ -68,6 +82,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~3.28.0',
+            'ember-cli': '^4.8.1',
           },
           ember: {
             edition: 'classic',
